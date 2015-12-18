0302135f81b2c6ab8305adff1c82cd7d7fffd634
Nao Pross <np.jun28@gmail.com>
2015-12-18 23:28:02 +0100

    Arduino Coffee code

---
+++ _02_coffee_machine/src/arduino/.build_ano/megaADK/arduino/wiring.d
@@ -0,0 +1,5 @@
+.build_ano/megaADK/arduino/wiring.d .build_ano/megaADK/arduino/wiring.o: /usr/share/arduino/hardware/arduino/avr/cores/arduino/wiring.c \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/wiring_private.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/avr/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/avr/variants/mega/pins_arduino.h
