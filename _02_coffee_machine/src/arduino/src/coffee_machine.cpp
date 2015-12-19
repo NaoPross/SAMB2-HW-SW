4667bb101e85f34df0790c92adcd5d41dd3e9e8f
Nao Pross <np.jun28@gmail.com>
2015-12-19 03:37:06 +0100

    Formatting

--- _02_coffee_machine/src/arduino/src/coffee_machine.cpp
+++ _02_coffee_machine/src/arduino/src/coffee_machine.cpp
@@ -1,33 +1,33 @@
-/* PINS                                     CHEAT SHEET
- * @-----------------------------------@    @--------------@
- * | AtMega2560 PORTC Wiring           |    | Binary | Hex |
- * @-----------------------------------@    @--------------@
- * | Bit | Pin | Connected To | Mode   |    | 0b0000 | 0x0 |
- * @-----------------------------------@    | 0b0001 | 0x1 |
- * |  7  |  30 | 1 chf        | INPUT  |    | 0b0010 | 0x2 | 
- * |  6  |  31 | 50 cts       | INPUT  |    | 0b0100 | 0x5 |
- * |  5  |  32 | 20 cts       | INPUT  |    | 0b1000 | 0x8 |
+/* PINS                                                
+ * @-----------------------------------@    @--------------@--------------@
+ * | AtMega2560 PORTC Wiring           |    | Binary | Hex |              |
+ * @-----------------------------------@    @--------------@              |
+ * | Bit | Pin | Connected To | Mode   |    | 0b0000 | 0x0 |    CHEAT     |
+ * @-----------------------------------@    | 0b0001 | 0x1 |              |
+ * |  7  |  30 | 1 chf        | INPUT  |    | 0b0010 | 0x2 |    SHEET     |
+ * |  6  |  31 | 50 cts       | INPUT  |    | 0b0100 | 0x5 |              |
+ * |  5  |  32 | 20 cts       | INPUT  |    | 0b1000 | 0x8 |              |
  * |  4  |  33 | 10 cts       | INPUT  |    |--------|-----|--------------@
  * |-----|-----|--------------|--------|    | 0b1111 | 0xF | 0b0110 | 0x6 |
  * |  3  |  34 | LED RED      | OUTPUT |    | 0b1110 | 0xE | 0b1010 | 0xA |
  * |  2  |  35 | LED BLUE     | OUTPUT |    | 0b1101 | 0xD | 0b1100 | 0xC |
  * |  1  |  36 | LED GREEN    | OUTPUT |    | 0b1011 | 0xB | 0b1110 | 0xE |
- * |  0  |  37 | Buy Coffee   | INPUT  |    | 0b0111 | 0x7 |        |     |
+ * |  0  |  37 | Buy Coffee   | INPUT  |    | 0b0111 | 0x7 | ////// | /// |
  * @-----------------------------------@    @--------------@--------------@
  * 
- * Data Direction Register C
+ * Data Direction Register C `DDRC`
  * 
  * This register sets the pin mode of every pin connected to the
  * port C. Every bit can be either 0 or 1, to be set as OUTPUT or
  * INPUT.
  *
- * Port C Data Register
+ * Port C Data Register `PORTC`
  *
  * This register can be used to set the value of every pin connected
  * to the port C. For every bit, 1 will turn the output on 0 will turn
  * it off.
  * 
- * Port C Input Pins Address
+ * Port C Input Pins Address `PINC`
  *
  * This register contains the actual value measured on the port C.
  */
@@ -54,7 +54,6 @@ void setup() {
 }
 
 void loop() {
-    
     // Ignore the output ports value
     int input_status = PINC & 0xF1;
 
@@ -70,19 +69,15 @@ void loop() {
         case 0x10:
             credit += 10;
             break;
-            
         case 0x20:
             credit += 20;
             break;
-
         case 0x40:
             credit += 50;
             break;
-
         case 0x80:
             credit += 100; 
             break;
-            
         case 0x01:
             if (credit >= COFFEE_PRICE) {
                 blink(0xFA, 4, 250);
@@ -95,7 +90,7 @@ void loop() {
         }
         
         if (credit >= COFFEE_PRICE) {
-            blink(0xFC, 1, 5);              // Blink is used to set the color
+            blink(0xFC, 1, 5); 
         }
         else {
             blink(0xF6, 1, 5);
